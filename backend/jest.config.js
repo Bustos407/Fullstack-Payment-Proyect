c7452201438e9517c956e0ecef985f89cf1b517c
Bustos407 <bustosmoralesantiago@gmail.com>
2026-03-06 17:51:14 -0500

    chore: initial backend config (package.json, tsconfig, jest)

---
+++ backend/jest.config.js
@@ -0,0 +1,30 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.spec.(t|j)s',
+    '!main.ts',
+    '!app.module.ts',
+    '!**/infrastructure/typeorm/*.entity.ts',
+  ],
+  coverageDirectory: '../coverage',
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
